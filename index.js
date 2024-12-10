cb312a6b2240955fd1e34ee05dc21a7659db9da0
sabir <sabir@root.root>
2024-12-10 22:16:17 +0530

    commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const strings = ["apple", "banana", "cat", "dog", "elephant"];
+const numbers = [1, 3, 5, 7, 9, 10, 12, 15, 20];
 
-const result = strings.filter((string) => {
-  return string.length > 5;
+const result = numbers.filter((number) => {
+  return number % 3 == 0 || number % 5 == 0;
 });
 console.log(result);
