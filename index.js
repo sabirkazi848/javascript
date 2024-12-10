e2ddb82e0601a4d5b1fb1592c9ba4083f3524bbc
sabir <sabir@root.root>
2024-12-10 22:11:54 +0530

    commit

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const users = [
-  { name: "Alice", isActive: true },
-  { name: "Bob", isActive: false },
-  { name: "Charlie", isActive: true },
-  { name: "David", isActive: false },
+const products = [
+  { name: "Laptop", price: 1200 },
+  { name: "Smartphone", price: 400 },
+  { name: "Tablet", price: 250 },
+  { name: "Monitor", price: 300 },
 ];
 
-const result = users.filter((user) => {
-  return user["isActive"];
+const result = products.filter((product) => {
+  return product["price"] >= 100 && product["price"] <= 500;
 });
 
 console.log(result);
